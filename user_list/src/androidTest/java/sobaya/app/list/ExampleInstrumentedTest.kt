c9088668d12bd712371bb9c4a2a111e0b8778335
shimoju <soba.ha.kenkou@gmail.com>
2022-04-14 21:30:01 +0900

    compileSdk version up

--- user_list/src/androidTest/java/sobaya/app/list/ExampleInstrumentedTest.kt
+++ user_list/src/androidTest/java/sobaya/app/list/ExampleInstrumentedTest.kt
@@ -1,12 +1,21 @@
 package sobaya.app.list
 
+import androidx.compose.material.Text
+import androidx.navigation.compose.rememberNavController
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.DeviceConfig.Companion
+import app.cash.paparazzi.Paparazzi
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
+import sobaya.app.composables.lv3_organisms.ListCard
+import sobaya.app.list.view.UserListSuccessView
+import sobaya.app.repository.model.User
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +24,14 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var paparazzi = Paparazzi(deviceConfig = DeviceConfig.PIXEL_3A)
+
     @Test
     fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("sobaya.app.list.test", appContext.packageName)
+        paparazzi.snapshot {
+            ListCard(onClick = {}, login = "sobaya")
+        }
     }
 }
